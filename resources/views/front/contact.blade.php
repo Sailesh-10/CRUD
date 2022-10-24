04a734bc11940a27cfb0d48719b70929a1077db2
smarika <smarikakarki17@gmail.com>
2022-10-24 15:51:52 +1100

    update on contact page

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -20,7 +20,7 @@
             <div class="info-item  d-flex flex-column justify-content-center align-items-center">
               <i class="bi bi-map"></i>
               <h3>Our Address</h3>
-              <p>A108 Adam Street, New York, NY 535022</p>
+              <p><address ><a href = "https://www.google.com.au/maps/place/EXTRATECH/@-33.9666098,151.1039312,17z/data=!3m1!5s0x6b12b9bd3751a199:0xee917e878a4b0dcf!4m13!1m7!3m6!1s0x6b12b9bd39b8e5db:0xb95792a42590004a!2s10+Park+Rd,+Hurstville+NSW+2220!3b1!8m2!3d-33.9666143!4d151.1061199!3m4!1s0x6b12b9a3a1ca51c9:0x60124f35094d0e21!8m2!3d-33.9666143!4d151.1061199" target = "blank">10 Park Road, Hurstville <br>NSW 2220, Sydney Australia </a></address></p>
             </div>
           </div><!-- End Info Item -->
 
@@ -28,7 +28,7 @@
             <div class="info-item d-flex flex-column justify-content-center align-items-center">
               <i class="bi bi-envelope"></i>
               <h3>Email Us</h3>
-              <p>contact@example.com</p>
+              <p><a href="mailto: info@extratechs.com.au">info@extratechs.com.au</a></p>
             </div>
           </div><!-- End Info Item -->
 
@@ -43,35 +43,6 @@
         </div>
 
         <div class="row gy-4 mt-1">
-
-          <div class="col-lg-6 ">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
-          </div><!-- End Google Maps -->
-
-          <div class="col-lg-6">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row gy-4">
-                <div class="col-lg-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                </div>
-                <div class="col-lg-6 form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-          </div><!-- End Contact Form -->
 
         </div>
 
