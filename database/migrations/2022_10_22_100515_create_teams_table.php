4a2bd0f8f53382fcf22b76401c1b93b2606f346f
Sailesh Khadka <sailesh@192-168-1-111.tpgi.com.au>
2022-10-28 01:54:59 +1100

    All complete

--- database/migrations/2022_10_22_100515_create_teams_table.php
+++ database/migrations/2022_10_22_100515_create_teams_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->string('image');
-            $table->string('name'); 
-            $table->string('position')->nullable(); 
+            $table->string('name');
+            $table->string('position')->nullable();
             $table->string('description')->nullable();
             $table->tinyInteger('status')->default('0')->nullable();
             $table->timestamps();
